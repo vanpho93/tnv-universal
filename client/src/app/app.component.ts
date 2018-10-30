943fc06cb25e4188b7b6f6dbce662200555fcb5c
Van Pho Nguyen <vanpho02@gmail.com>
2018-10-30 11:53:43 +0700

    ssr

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+const TODO_KEY = makeStateKey('TODO_KEY');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'tnv-universal';
+  data = null;
+
+  constructor(private http: Http, private state: TransferState) {}
+
+  ngOnInit() {
+    this.data = this.state.get(TODO_KEY, null);
+    if (this.data) { return; }
+    const URL = 'https://jsonplaceholder.typicode.com/todos/1';
+    this.http.get(URL)
+    .toPromise()
+    .then(res => res.json())
+    .then(resJson => {
+      this.data = resJson;
+      this.state.set(TODO_KEY, resJson);
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports:[
- CommonModule,
-NgtUniversalModule,
- 
-    
+  imports: [
+    CommonModule,
+    NgtUniversalModule,
+    HttpModule,
+    BrowserTransferStateModule
   ],
   providers: [],
 })
+
 export class AppModule { }
